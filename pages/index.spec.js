02b11adb77ac4924806588ee9243604a72429b61
Binod Nepali <nepalibinod9@gmail.com>
2021-11-02 21:52:01 +0100

    fix (Unit Test): replace heading text in the unit test

--- pages/index.spec.js
+++ pages/index.spec.js
@@ -4,8 +4,6 @@ import Index from "./index";
 
 test("renders deploy link", () => {
   const { getByText } = render(<Index />);
-  const linkElement = getByText(
-    /Instantly deploy your Next\.js site to a public URL with Vercel\./
-  );
+  const linkElement = getByText(/Next.js Material UI Template/);
   expect(linkElement).toBeInTheDocument();
 });
